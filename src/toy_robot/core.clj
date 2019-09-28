5d9bf988b9a13ef1462da7c579868024ca84be14
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-09-28 10:01:59 +1000

    State update for place

--- src/toy_robot/core.clj
+++ src/toy_robot/core.clj
@@ -14,7 +14,7 @@
 
 (defn report [] (str (:direction @app-state) "," (get-in @app-state [:coords :x]) "," (get-in @app-state [:coords :y])))
 
-(defn re-match-place [str]  (re-matches #"PLACE (\d),(\d),(NORTH|EAST|WEST|SOUTH)" str ))
+(defn re-match-place [str]  (re-matches #"PLACE (\d),(\d),(NORTH|EAST|WEST|SOUTH)" str))
 
 (defn validate-report [str] (= str "REPORT"))
 (defn validate-quit [str] (= str "QUIT"))
@@ -23,10 +23,14 @@
 (defn validate-right [str] (= str "RIGHT"))
 (defn validate-place [str] (some? (re-match-place str)))
 
+(defn place [args]
+  (let [[str x y direction] args]
+    (swap! app-state merge {:direction direction :coords {:x  (Integer. x) :y (Integer. y)}})))
+
 (defn main-loop []
   (loop [in (read-line)]
     (cond
-      (validate-place in) (recur (read-line))
+      (validate-place in) (do (place (re-match-place in)) (recur (read-line)))
       (validate-left in) (recur (read-line))
       (validate-right in) (recur (read-line))
       (validate-move in) (recur (read-line))
@@ -36,4 +40,5 @@
 
 (defn -main
   [& _]
+  (println "Booting up Toy Robot...beep...bop...")
   (main-loop))
