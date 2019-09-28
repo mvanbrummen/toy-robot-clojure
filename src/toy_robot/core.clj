82cc280239a820b6f0097ac625d1167420cab860
Michael Van Brummen <michaelvanbrummen@gmail.com>
2019-09-28 10:52:09 +1000

    Working

--- src/toy_robot/core.clj
+++ src/toy_robot/core.clj
@@ -3,7 +3,28 @@
             [clojure.string :refer [lower-case upper-case replace]])
   (:gen-class))
 
+;; board size ie 5x5
+(def board-size 5)
+
+;; directions
+
 (def direction [:north :east :south :west])
+
+(defn direction-left [dir]
+  (case dir
+    :north :west
+    :east :north
+    :south :east
+    :west :south
+    :else dir))
+
+(defn direction-right [dir]
+  (case dir
+    :north :east
+    :east :south
+    :south :east
+    :west :north
+    :else dir))
 
 (defn string->direction [str]
   (-> str
@@ -14,8 +35,9 @@
   (-> dir
       (str)
       (upper-case)
-      (replace #":" "")
-      ))
+      (replace #":" "")))
+
+;; app state
 
 (def app-state (atom {:direction nil :coords {:x nil :y nil}}))
 
@@ -25,9 +47,42 @@
    (nil? (get-in @app-state [:coords :x]))
    (nil? (get-in @app-state [:coords :y]))))
 
+(defn re-match-place [str]  (re-matches #"PLACE (\d),(\d),(NORTH|EAST|WEST|SOUTH)" str))
+
+;; commands 
+
 (defn report [] (str (direction->string (:direction @app-state)) "," (get-in @app-state [:coords :x]) "," (get-in @app-state [:coords :y])))
 
-(defn re-match-place [str]  (re-matches #"PLACE (\d),(\d),(NORTH|EAST|WEST|SOUTH)" str))
+(defn place [args]
+  (let [[str x y direction] args]
+    (swap! app-state merge {:direction (string->direction direction) :coords {:x  (Integer. x) :y (Integer. y)}})))
+
+(defn left [] (swap! app-state update-in [:direction] direction-left))
+(defn right [] (swap! app-state update-in [:direction] direction-right))
+
+(defn update-y [y] (swap! app-state assoc-in [:coords :y] y))
+(defn update-x [x] (swap! app-state assoc-in [:coords :x] x))
+
+(defn valid-coords? [x y]
+  (and
+   (>= x 0)
+   (>= y 0)
+   (< x board-size)
+   (< y board-size)))
+
+(defn move []
+  (let [direction (:direction @app-state)
+        x (get-in @app-state [:coords :x])
+        y (get-in @app-state [:coords :y])]
+
+  (case direction
+    :north (do (when (valid-coords? x (inc y))  (update-y (inc y))))
+    :east (do (when (valid-coords? (inc x) y)  (update-x (inc x))))
+    :south (do (when (valid-coords? x (dec y))  (update-y (dec y))))
+    :west (do (when (valid-coords? (dec x) y)  (update-x (dec x)))))))
+
+;; validate commands
+
 
 (defn validate-report [str] (= str "REPORT"))
 (defn validate-quit [str] (= str "QUIT"))
@@ -36,20 +91,20 @@
 (defn validate-right [str] (= str "RIGHT"))
 (defn validate-place [str] (some? (re-match-place str)))
 
-(defn place [args]
-  (let [[str x y direction] args]
-    (swap! app-state merge {:direction (string->direction direction) :coords {:x  (Integer. x) :y (Integer. y)}})))
+;; loop
 
 (defn main-loop []
   (loop [in (read-line)]
     (cond
       (validate-place in) (do (place (re-match-place in)) (recur (read-line)))
-      (validate-left in) (recur (read-line))
-      (validate-right in) (recur (read-line))
-      (validate-move in) (recur (read-line))
+      (validate-left in) (do (left) (recur (read-line)))
+      (validate-right in) (do (right) (recur (read-line)))
+      (validate-move in) (do (move) (recur (read-line)))
       (validate-quit in) (System/exit 0)
-      (validate-report in) (if (app-state-nil?) (recur (read-line)) (println (report)))
+      (validate-report in) (if (app-state-nil?) (recur (read-line)) (do (println (report)) (recur (read-line))))
       :else (recur (read-line)))))
+
+;; main func
 
 (defn -main
   [& _]
